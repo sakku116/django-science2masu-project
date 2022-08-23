73194ebcd0e3d31aaf15733828d9b3089bc170d1
Zakky <kycraft1164@gmail.com>
2022-08-23 19:53:58 +0700

    update

--- modules/custom_log_info.py
+++ modules/custom_log_info.py
@@ -1,14 +1,6 @@
 from termcolor import colored
-from datetime import datetime
 
-def customLogInfo(path, view_name, user_agent, user):
-    path = f'"{path}"'
-    user = f'@{user}'
-    device = f"{user_agent[user_agent.find('(')+1:user_agent.find(')')]}"
-
-    now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-
+def customLogInfo(path, view_name, user_agent, user, device, dt_string):
     text = f"=> ({dt_string}) = {user} | {path} ({view_name}) | {device}"
 
     if user == '@AnonymousUser':
