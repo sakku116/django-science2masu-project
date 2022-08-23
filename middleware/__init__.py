13e5f269fd478c623ce125e8a668b57a304cb2e9
Zakky <kycraft1164@gmail.com>
2022-08-23 19:55:13 +0700

    write every log with given max number each request

--- middleware/__init__.py
+++ middleware/__init__.py
@@ -1,4 +1,7 @@
-from modules import customLogInfo
+from modules import colorizeString
+from datetime import datetime
+from utils import writeLog
+import threading
 
 class RequestMonitor:
     def __init__(self, get_response):
@@ -7,9 +10,24 @@ class RequestMonitor:
     def __call__(self, request):
         response = self.get_response(request)
         
-        full_path = request.get_full_path()
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")        
         user_agent = request.META['HTTP_USER_AGENT']
-        user = request.user
-        print(customLogInfo(full_path, request.resolver_match.view_name, user_agent, user))
+        view_name = request.resolver_match.view_name
+        full_path = f'"{request.get_full_path()}"'
+        user = f'@{request.user}'
+        device = f"{user_agent[user_agent.find('(')+1:user_agent.find(')')]}"
+
+        log_string = f"=> ({dt_string}) = {user} | {full_path} ({view_name}) | {device}"
+        max_log_line = 5
+        
+        if user == '@AnonymousUser':
+            color = 'white'
+        else:
+            color = 'yellow'
+        print(colorizeString(log_string, color, reverse=True))
+
+        write_log_thread = threading.Thread(target=writeLog, args=(log_string, 10,))
+        write_log_thread.start()
 
         return response
