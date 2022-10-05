c7e1b2bf08183bd49709cdaa6e954dee3644df78
Zakky <88846365+sakku116@users.noreply.github.com>
2022-10-05 21:35:47 +0700

    save log in db instead file

--- middleware/__init__.py
+++ middleware/__init__.py
@@ -1,7 +1,7 @@
 from modules import colorizeString
 from datetime import datetime
 from modules import writeLog
-import threading
+from api_app.models import RequestLog
 
 class RequestMonitor:
     def __init__(self, get_response):
@@ -13,14 +13,17 @@ class RequestMonitor:
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
         user_agent = request.META['HTTP_USER_AGENT']
-        view_name = request.resolver_match.view_name
+        try:
+            view_name = request.resolver_match.view_name
+        except:
+            view_name = "unknown"
         full_path = f'"{request.get_full_path()}"'
         user = f'@{request.user}'
         device = f"{user_agent[user_agent.find('(')+1:user_agent.find(')')]}"
 
+        # print log
         log_string = f"=> ({dt_string}) = {user} | {request.method} {full_path} ({view_name}) | {device}"
-        max_log_line = 5
-
+        max_log = 500
         if user == '@AnonymousUser':
             color = 'white'
         else:
@@ -31,8 +34,19 @@ class RequestMonitor:
         if "bot" in user_agent.lower() or "media" in user_agent.lower() or "google" in user_agent.lower():
             pass
         else:
-            # save log with threading
-            write_log_thread = threading.Thread(target=writeLog, args=(log_string, 100,))
-            write_log_thread.start()
+            # limit
+            logs_total = RequestLog.objects.all().count()
+            if logs_total >= max_log:
+                first_log = RequestLog.objects.first()
+                first_log.delete()
+
+            # save
+            new_log = RequestLog.objects.create(
+                ViewName = view_name,
+                Path = full_path,
+                Username = user,
+                Device = device,
+            )
+            new_log.save()
 
         return response
